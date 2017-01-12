e0d5866a4d6962533f3a7e0f0fd1c30d8c36e99c
Jeremy <acnajcadcac.yahoo.com>
2017-01-12 16:39:58 -0600

    Added user input

--- API_Test.py
+++ API_Test.py
@@ -1,14 +1,23 @@
 import json, requests
 
-# getting the user for the movie
+movieTitle = input("Enter a movie to search for:")
+
+# getting the url for the movie
 try:
-    url = "http://www.omdbapi.com/?t=star wars"
+    url = "http://www.omdbapi.com/?"
 
 except ConnectionError:
     print("Website unreachable")
+
+
+parameters = dict(
+    t=movieTitle
+)
+
+omdbResponse = requests.get(url=url, params=parameters)
 # getting the response
-omdbResponse = requests.get(url)
+
 # getting the text from the request
 rating = json.loads(omdbResponse.text)
 # printing the results
-print("Rating for Star Wars", rating['imdbRating'])
+print("Rating for", rating["Title"], rating['imdbRating'])
